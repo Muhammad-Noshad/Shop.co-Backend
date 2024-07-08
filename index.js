29ab6183cc8f467fe35e3ce8c16a0558a82dcdd3
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-07-08 18:13:19 +0500

    deploy

--- index.js
+++ index.js
@@ -18,17 +18,11 @@ const PORT = process.env.PORT || 8000;
 connectDB(process.env.DB_URL);
 
 const corsOptions = {
-  origin: 'https://shop-co-blond.vercel.app',
+  origin: '*',
   credentials: true,
   methods: ['POST', 'PATCH', 'DELETE', 'GET']
 };
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
 app.use(cors(corsOptions));
 app.use(limiter);
 app.use(express.urlencoded({ extended: false }));
