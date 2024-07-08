b69f526dd16c0249620472552d02dd2525a1651a
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-07-08 21:23:16 +0500

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const { connectDB } = require("./connectDB");
-const mongoose = require('mongoose');
 
 const cors = require('cors')
 const cookieParser = require("cookie-parser");
@@ -16,7 +15,7 @@ const profileRouter = require("./routers/profile");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-//connectDB(process.env.DB_URL);
+connectDB(process.env.DB_URL);
 
 const corsOptions = {
   origin: 'https://shop-co-blond.vercel.app', 
@@ -29,16 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/user/sign-in", (req,res) => {
-  return res.json({ success: true });
+app.get("/token/verify", (req,res) => {
+  return res.json({ status: "success" });
 })
 app.use("/token", tokenRouter);
 app.use("/user", userRouter);
 app.use("/profile", profileRouter);
 
-  mongoose.connect(process.env.DB_URL)
-  .then(() => {
-    console.log("MongoDB connected successfully!")
-    app.listen(PORT, () => console.log("Server started at PORT:", PORT));
-  })
-  .catch((err) => console.log("MongoDB connection error!", err));
+app.listen(PORT, () => console.log("Server started at PORT:", PORT));
