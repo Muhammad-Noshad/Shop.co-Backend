b8eee684f41286df407fcf082af8502272bea76c
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-07-08 18:07:24 +0500

    deploy

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const corsOptions = {
   methods: ['POST', 'PATCH', 'DELETE', 'GET']
 };
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 app.use(cors(corsOptions));
 app.use(limiter);
 app.use(express.urlencoded({ extended: false }));
