415d4207537527198ac89471990fb61788a1823f
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-07-08 19:15:08 +0500

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { connectDB } = require("./connectDB");
 
-const cors = require('cors')
+//const cors = require('cors')
 const cookieParser = require("cookie-parser");
 const rateLimit = require('express-rate-limit');
 
@@ -23,7 +23,7 @@ connectDB(process.env.DB_URL);
 //    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 // };
 
-app.use(cors());
+//app.use(cors());
 app.use(limiter);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
